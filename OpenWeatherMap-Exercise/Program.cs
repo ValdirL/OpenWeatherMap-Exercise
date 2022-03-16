7e5fca0bbc85d7b5cf5a76fe81e8198d1f70e7d5
Valdir Lima <valdirlima508@gmail.com>
2022-03-16 16:15:55 -0400

    initial commit

--- OpenWeatherMap-Exercise/Program.cs
+++ OpenWeatherMap-Exercise/Program.cs
@@ -9,18 +9,30 @@ namespace OpenWeatherMap_Exercise
         static void Main(string[] args)
         {
             // TODO: Create an instance of the HttpClient Class called client
+            var client = new HttpClient();
 
             // TODO: Ask for the users API Key and store that in a variable called "api_Key"
+            Console.WriteLine("Please enter your API key: ");
+            var api_Key = Console.ReadLine();
 
             // TODO: Ask the user for their city name and store that in a variable called "city_name"
+            Console.WriteLine("Please enter your city name: ");
+            var city_name = Console.ReadLine();
 
             // TODO: Create a variable to store the URL (use String Interpolation for the {city_name} and {api_Key}  HINT: Make sure to use the "imperial" measurement endpoint
+            var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
 
             // TODO: Create a variable to store the response from your GET request to that URL from above  HINT: Don't forget to call .Result 
+            var response = client.GetStringAsync(weatherURL).Result;
 
             // TODO: Create a variable to store the formattedResponse after you JObject.Parse() the response from above
+            var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
 
             // TODO: Write out the current temperature in degrees Fahrenheit
+            Console.WriteLine(formattedResponse);
+            Console.WriteLine();
+            
+            
         }
     }
 }
